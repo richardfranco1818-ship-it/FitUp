a63b3216cbf11d009bf8202d553967fb7546d7e5
Richard <richardfranco1818@gmail.com>
2025-12-07 19:00:10 -0600

    Se agergo todo sobre la base de datos

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4wa5tOCR8D0R0kZVZJxWuv4aCLyVwu_s",
+  authDomain: "fitup-491d6.firebaseapp.com",
+  projectId: "fitup-491d6",
+  storageBucket: "fitup-491d6.firebasestorage.app",
+  messagingSenderId: "741684257944",
+  appId: "1:741684257944:web:c8610e8521efc45a74dced"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar servicios
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
